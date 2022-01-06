ef20aed1c44ce22d5011056d0cc845cbd0a78c71
akoppela <akoppela@gmail.com>
2022-01-06 13:28:14 +0100

    Update Go version and introduce Rust.

---
+++ go/go.mod
@@ -0,0 +1,5 @@
+module github.com/akoppela/fin-calc
+
+go 1.16
+
+require golang.org/x/text v0.3.7
--- shell.nix
+++ shell.nix
@@ -1,7 +1,12 @@
-{ pkgs ? import (fetchTarball "https://github.com/NixOS/nixpkgs/tarball/20.09") { }
+{ pkgs ? import (fetchTarball "https://github.com/NixOS/nixpkgs/tarball/21.11") { }
 }:
 pkgs.mkShell {
   buildInputs = [
+    # GO
     pkgs.go
+
+    # RUST
+    pkgs.rustc
+    pkgs.cargo
   ];
 }
